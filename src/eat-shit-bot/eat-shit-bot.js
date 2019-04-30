d31496d2bcae16cd85e46427b8783d034b5e6010
bws9000 <armorsoft@gmail.com>
2019-04-30 18:45:55 -0400

    moved credentials

--- src/eat-shit-bot/eat-shit-bot.js
+++ src/eat-shit-bot/eat-shit-bot.js
@@ -3,7 +3,13 @@
 var _ = require('lodash');
 var merge = require('lodash/merge');
 var Twit = require('twit');
-//var credentials = require('./credentials');
+
+var credentials = {
+	consumer_key: process.env.API_KEY,
+	consumer_secret: process.env.API_SECRET_KEY,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
 
 /**
  * EatShitBot constructor description
