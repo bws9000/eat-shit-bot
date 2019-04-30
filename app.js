fdf59d1c1e3ebd038c1cae3815aa24a23b9e84a2
bws9000 <armorsoft@gmail.com>
2019-04-30 19:32:24 -0400

    changed phrase

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var eatShitBot = require('./src/eat-shit-bot/eat-shit-bot');
 var App = (function() {
 
 	var Bot;
-	var phrase = 'Musee Pompidou';
+	var phrase = 'Satan';
 
 	var initialize = function() {
 		Bot = new eatShitBot();
